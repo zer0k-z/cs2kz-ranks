ba99bf213b18ffca852878ed566e2ae56423e3f6
zer0.k <zer0kams@gmail.com>
2026-01-18 05:46:29 +0100

    Fix common.py indentation inconsistencies

--- common.py
+++ common.py
@@ -3,10 +3,10 @@ import numpy as np
 from scipy import stats
 
 def get_distribution_points_portion_under_50(time, wr_time, tier):
-    return ((1+np.exp((2.1 - 0.25 * tier) * -0.5))/(1+np.exp((2.1 - 0.25 * tier) * (time/wr_time-1.5))))
+	return ((1+np.exp((2.1 - 0.25 * tier) * -0.5))/(1+np.exp((2.1 - 0.25 * tier) * (time/wr_time-1.5))))
 
 def get_dist_points_portion(time, wr_time, dist: stats.rv_continuous, tier, top_scale, total):
-    if total < 50:
-        return get_distribution_points_portion_under_50(time, wr_time, tier)
-    else:
-        return (dist.sf(time)) / top_scale
+	if total < 50:
+		return get_distribution_points_portion_under_50(time, wr_time, tier)
+	else:
+		return (dist.sf(time)) / top_scale
